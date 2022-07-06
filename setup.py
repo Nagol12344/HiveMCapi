7c490f99313b361d5139d12325e6f5885e50f851
Logan Someone <loganqqq@outlook.com>
2022-07-06 18:41:24 -0400

    verison bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name = 'hivemcapi',         # How you named your package folder (MyLib)
   packages = ['hivemcapi'],   # Chose the same as "name"
-  version = 'v2.1',      # Start with a small number and increase it with every change you make
+  version = 'v3.0b1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A api rapper for the hivemc api',   # Give a short description about your library
   long_description=long_description,
